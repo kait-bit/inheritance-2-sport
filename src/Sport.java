f6c8d08a4cf2df475cb16b49a990bc2dfb8a8acb
kaiti <122762348+kait-bit@users.noreply.github.com>
2025-03-02 01:44:33 -0600

    removed problem description, messaged on canvas

--- src/Sport.java
+++
@@ -1,5 +0,0 @@
-import java.util.ArrayList;
-
-public interface Sport {
-    ArrayList<String> players();
-}
--- src/Tennis.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public record Tennis(ArrayList<String> players) implements Sport {
-    @Override public String toString() {
-        return "Tennis players: " + players;
-    }
-}
